4b6704687252189845363c7e7344c19836a6ade1
mpryor <mapryor1991@yahoo.com>
2021-08-12 15:20:26 -0500

    added more to the setup-cluster script

--- cluster/setup-cluster.sh
+++ cluster/setup-cluster.sh
@@ -2,3 +2,6 @@ cd metallb
 ./install.sh
 cd ..
 
+kubectl apply -f metrics-server/components.yml
+kubectl apply -f getting-started-k8s/Pods/pod.yml
+kubectl apply -f getting-started-k8s/Services/svc-lb.yml
