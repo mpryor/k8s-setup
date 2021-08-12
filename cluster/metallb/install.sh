1d9785a1f8285a884169ef5076df374980586f39
mpryor <mapryor1991@yahoo.com>
2021-08-12 12:14:05 -0500

    Added cluster setup

---
+++ cluster/metallb/install.sh
@@ -0,0 +1,9 @@
+# actually apply the changes, returns nonzero returncode on errors only
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/metallb.yaml
+
+kubectl apply -f config.yml
