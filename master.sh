4711e415643b85243d9078f499321787479d842d
mpryor <mapryor1991@yahoo.com>
2021-08-13 09:24:17 -0500

    Made setup-cluster.sh executable, and removed the cluster bootstrap from the master setup. Need to find another/better way to do this.

--- master.sh
+++ master.sh
@@ -25,9 +25,3 @@ kubectl apply -f /tmp/calico-defined.yaml
 echo KUBELET_EXTRA_ARGS=--node-ip=$MASTER_IP  > /etc/default/kubelet
 systemctl restart networking
 systemctl restart kubelet
-
-git clone https://github.com/mpryor/k8s-setup
-cd k8s-setup
-cd cluster
-chmod +x setup-cluster.sh
-./setup-cluster.sh
